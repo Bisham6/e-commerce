a17e1090296a10d9af8659428468d2da137f988a
Bisham <bisham65@gmail.com>
2022-04-04 11:22:44 +0530

    new change

--- src/app/produd-detail/produd-detail.component.ts
+++ src/app/produd-detail/produd-detail.component.ts
@@ -24,6 +24,7 @@ export class ProdudDetailComponent implements OnInit {
     this.img= this.active.snapshot.params['img']
     this.des= this.active.snapshot.params['des']
     this.price= this.active.snapshot.params['price']
+    this.image("id","img")
   }
 
   addProduct(){
@@ -39,8 +40,25 @@ export class ProdudDetailComponent implements OnInit {
     this.cartService.createPost(payload);
     console.log(payload)
     
-
-
   }
+  image(id:string,id2:string){ 
+    var zoom:any=document.getElementById(id) 
+    var zimg:any=document.getElementById(id2) 
+    zoom.addEventListener("mousemove",function(event:any){ 
+        let clientX=event.clientX-zoom.offsetLeft; 
+        let clientY=event.clientY-zoom.offsetTop 
+        let w=zoom.offsetWidth; 
+        let h=zoom.offsetHeight; 
+        clientX=clientX/w *100; 
+        clientY=clientY/h *100; 
+        zimg.style.transform='translate(-'+clientX+'%,-'+clientY+'%) scale(2)' 
+    }) 
+    zoom.addEventListener("mouseleave",function(){ 
+        zimg.style.transform='translate(-50%,-50%) scale(1)' 
+
+    }) 
+  }
+
+
 
 }
